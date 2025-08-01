cb61f4edba0194ce5a7fd22baf510f7c3d8d15a3
anuragsamota <anuragsamotasamota@gmail.com>
2025-08-01 11:39:24 +0530

    added copy button

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // DOM elements
-const chatDiv = document.getElementById("chat-div");
-const connStatusDiv = document.getElementById("self-conn-status");
+const chatDiv = document.getElementById("chat-div")
+const connStatusDiv = document.getElementById("self-conn-status")
 const connId = document.getElementById("conn-id")
-const peerIdInput = document.getElementById("peers-id-inp");
-const joinPeerButton = document.getElementById("join-peer-btn");
-const connectedPeerDiv = document.getElementById("connected-peers");
+const peerIdInput = document.getElementById("peers-id-inp")
+const joinPeerButton = document.getElementById("join-peer-btn")
+const connectedPeerDiv = document.getElementById("connected-peers")
 const sendMessageButton = document.getElementById("send-msg-btn")
 const messageInput = document.getElementById('msg-inp')
+const copyButton = document.getElementById('copy-btn')
 
 
 
 // Global variables
-const peer = new Peer();
-const connections = [];
+const peer = new Peer()
+const connections = []
 
 // functions
 function connectToOtherPeer() {
@@ -21,20 +22,20 @@ function connectToOtherPeer() {
       const connection = peer.connect(peerIdInput.value);
       connection.on("open", () => {
         connections.push(connection);
-        connectedPeerDiv.innerHTML = "";
+        connectedPeerDiv.innerHTML = ""
         connections.forEach((item) => {
-          connectedPeerDiv.insertAdjacentHTML("beforeend",'<div class="btn btn-outline btn-accent">'+ item.peer + "</div>");
+          connectedPeerDiv.insertAdjacentHTML("beforeend",'<div class="btn btn-outline btn-accent">'+ item.peer + "</div>")
           item.on("data", (data)=>{
             onMessageReceive(data)
           });
         });
-        peerIdInput.value = "";
+        peerIdInput.value = ""
       });
     } catch (error) {
       console.log(error);
     }
   } else {
-    console.log("Please enter a id");
+    console.log("Please enter a id")
   }
 }
 
@@ -51,7 +52,7 @@ function sendMessage(){
         connection.send(messageInput.value)
       })
       chatDiv.insertAdjacentHTML("beforeend", "<div class='chat chat-end'><div class='chat-bubble chat-bubble-secondary'>" +messageInput.value+"</div></div>")
-      messageInput.value = "";
+      messageInput.value = ""
       chatDiv.scrollTop = chatDiv.scrollHeight
 
     } catch (error) {
@@ -61,29 +62,36 @@ function sendMessage(){
   }
 }
 
+function copyToClipboard(){
+  let text = connId.innerText
+  navigator.clipboard.writeText(text)
+}
+
 
 
 
 
 // event listeners
-joinPeerButton.onclick = connectToOtherPeer;
-sendMessageButton.onclick = sendMessage;
+joinPeerButton.onclick = connectToOtherPeer
+sendMessageButton.onclick = sendMessage
+copyButton.onclick = copyToClipboard
 
 
 peer.on("open", (id) => {
   connStatusDiv.innerText = "Initialized Connection"
-  connId.innerText = id;
-});
+  connId.innerText = id
+  copyButton.removeAttribute("disabled")
+})
 
 peer.on("connection", (connection) => {
   connection.on("data", (data)=>{
     onMessageReceive(data)
   });
   connections.push(connection);
-  connectedPeerDiv.innerHTML = "";
+  connectedPeerDiv.innerHTML = ""
   connections.forEach((item) => {
-    connectedPeerDiv.insertAdjacentHTML("beforeend",'<div class="btn btn-outline btn-accent">'+ item.peer + "</div>");
-  });
-});
+    connectedPeerDiv.insertAdjacentHTML("beforeend",'<div class="btn btn-outline btn-accent">'+ item.peer + "</div>")
+  })
+})
 
 // initailizations
